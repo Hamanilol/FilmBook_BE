81e339f5d1636768789b88f3189488ad96e04d1c
MohamedHerz <mohamed.herz96@gmail.com>
2025-12-28 18:37:06 +0300

    commit to save changes before merging

--- models/favorited.js
+++ models/favorited.js
@@ -1,9 +1,9 @@
 const { Schema } = require("mongoose")
 
-const favoritedSchema = new mongoose.Schema(
+const favoritedSchema = new Schema(
   {
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    movie: {type: String, required: true }
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    movie: { type: String, required: true },
   },
   { timestamps: true }
 )
